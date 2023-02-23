273dd17246b10bd5de4f6c459ec40751f107b68b
CellineLin <celline_yasshinta_30rpl@student.smktelkom-mlg.sch.id>
2023-02-23 11:37:38 +0700

    Update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,9 @@
 import "@/styles/globals.css";
 // pages/_app.js
-import { Inter } from "@next/font/google";
+// import { Inter } from "@next/font/google0";
 
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }) {
   return (
     <main className={`${inter.variable} font-sans`}>
